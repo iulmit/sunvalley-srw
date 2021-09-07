af3c4690da3b6f54564aa03f8d144c451e128a03
Ken Hoo <60463234+mrkenhoo@users.noreply.github.com>
2021-09-07 22:13:12 +0200

    First version

---
+++ modules/ApplySystemReadinessLite.ps1
@@ -0,0 +1,17 @@
+#
+# Created by Liam Powell (gfelipe099)
+# ApplySystemReadiness module for sunvalley-srw
+#
+
+Import-Module BitsTransfer 2>&1
+Write-Host "==> Downloading O&O ShutUp10..."
+cd $env:TEMP
+Start-BitsTransfer -Source "https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe" -Destination OOSU10.exe 2>&1
+    
+Write-Host "    --> Downloading gfelipe099's O&O ShutUp10 privacy file..."
+Start-BitsTransfer -Source "https://raw.githubusercontent.com/gfelipe099/threshold-srb/master/ooshutup10.cfg" -Destination ooshutup10.cfg 2>&1
+
+Write-Host "    --> Applying privacy policies from O&O ShutUp10 privacy file..."
+.\OOSU10.exe ooshutup10.cfg /quiet
+cd $PSScriptRoot
+Remove-Module BitsTransfer
