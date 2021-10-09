cab18a01aa5369cda77c86194a0a09c75d18dfc6
Ken Hoo <60463234+mrkenhoo@users.noreply.github.com>
2021-10-09 20:10:49 +0100

    Fix typos

--- modules/ApplySystemReadiness.ps1
+++ modules/ApplySystemReadiness.ps1
@@ -325,8 +325,8 @@ $wof | ForEach-Object {
 
 Write-Host ""; Write-Host "==> Windows capabilities"
 $wc = (Get-WindowsCapability -Online).Name
-Write-Host ('        -> Found ' + $wc.Count + ' Windows capabilitities...')
+Write-Host ('        -> Found ' + $wc.Count + ' Windows capabilities...')
 $wc | ForEach-Object {
-    Write-Host "            - Removing capatiblity: $_..."
+    Write-Host "            - Removing capability: $_..."
     Remove-WindowsCapability -Name $_ -Online -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
 }
