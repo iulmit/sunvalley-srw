a336ff4aea80840e67b4e840fe29f076c2426bf8
Ken Hoo <60463234+mrkenhoo@users.noreply.github.com>
2021-10-09 20:11:43 +0100

    Added new modules

---
+++ modules/DisableAllWindowsOptionalFeatures.ps1
@@ -0,0 +1,11 @@
+﻿#
+# Created by Ken Hoo (mrkenhoo)
+# DisableAllWindowsOptionalFeatures.ps1 module for sunvalley-srw
+#
+
+$wof = (Get-WindowsOptionalFeature -FeatureName '*' -Online).FeatureName
+Write-Host ('==> Found ' + $wof.Count + ' Windows optional feature(s) on this system')
+$wof | ForEach-Object {
+    Write-Host "    -> Disable optional feature: $_..."
+    Disable-WindowsOptionalFeature -FeatureName $_ -Online -NoRestart -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+}
---
+++ modules/RemoveAllWindowsCapabilities.ps1
@@ -0,0 +1,11 @@
+﻿#
+# Created by Ken Hoo (mrkenhoo)
+# DisableAllWindowsOptionalFeatures.ps1 module for sunvalley-srw
+#
+
+$wc = (Get-WindowsCapability -Online).Name
+Write-Host ('==> Found ' + $wc.Count + ' Windows capabilities...')
+$wc | ForEach-Object {
+    Write-Host "    -> Removing capability: $_..."
+    Remove-WindowsCapability -Name $_ -Online -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+}
