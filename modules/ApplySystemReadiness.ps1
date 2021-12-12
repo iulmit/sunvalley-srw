fe956f5495decc58762b04529113d56816eb5036
Ken Hoo <60463234+mrkenhoo@users.noreply.github.com>
2021-12-12 09:31:59 +0000

    Script improvements, URL configuration file updated

--- modules/ApplySystemReadiness.ps1
+++ modules/ApplySystemReadiness.ps1
@@ -97,10 +97,10 @@ Write-Host "==> Downloading O&O ShutUp10..."
 cd $env:TEMP
 Start-BitsTransfer -Source "https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe" -Destination OOSU10.exe 2>&1
     
-Write-Host "    --> Downloading mrkenhoo's O&O ShutUp10 privacy file..."
-Start-BitsTransfer -Source "https://raw.githubusercontent.com/mrkenhoo/threshold-srb/master/ooshutup10.cfg" -Destination ooshutup10.cfg 2>&1
+Write-Host "    --> Downloading mrkenhoo's O&O ShutUp10 hardened configuration file..."
+Start-BitsTransfer -Source "https://raw.githubusercontent.com/mrkenhoo/sunvalley-srw/sunvalley-v3/telemetry_policies/hardened/ooshutup10.cfg" -Destination ooshutup10.cfg 2>&1
 
-Write-Host "    --> Applying privacy policies from O&O ShutUp10 privacy file..."
+Write-Host "    --> Applying policies from O&O ShutUp10 configuration file..."
 .\OOSU10.exe ooshutup10.cfg /quiet
 cd $PSScriptRoot
 Remove-Module BitsTransfer
