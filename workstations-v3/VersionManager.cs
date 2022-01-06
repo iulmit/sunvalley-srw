c652964f14477ec55a390e5e27bd4091285bd306
Ken Hoo <60463234+mrkenhoo@users.noreply.github.com>
2022-01-06 02:39:36 +0000

    Create VersionManager.cs

---
+++ workstations-v3/VersionManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace workstations_v3
+{
+    partial class VersionManager
+    {
+        public static string ShowCurrentVersion()
+        {
+            return "Current version: " + typeof(Program).Assembly.GetName().Version.ToString();
+        }
+    }
+}
