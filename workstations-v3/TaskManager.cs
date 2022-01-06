5a726524449b1951a191df889ea3eeeb290e205e
Ken Hoo <60463234+mrkenhoo@users.noreply.github.com>
2022-01-06 02:39:53 +0000

    Simplify code

--- workstations-v3/TaskManager.cs
+++ workstations-v3/TaskManager.cs
@@ -6,7 +6,7 @@ namespace workstations_v3
     public class TaskManager
     {
         public static void CreateTask(string TaskName, string ExecutableFile, string Arguments, string WorkingDirectory, string description, short NumberOfDays) {
-            TaskService ts = new TaskService();
+            TaskService ts = new();
             TaskDefinition td = ts.NewTask();
             
             td.RegistrationInfo.Description = description;
@@ -22,7 +22,7 @@ namespace workstations_v3
         }
         public static void RemoveTask(string TaskName)
         {
-            TaskService ts = new TaskService();
+            TaskService ts = new();
 
             ts.RootFolder.DeleteTask(TaskName);
         }
